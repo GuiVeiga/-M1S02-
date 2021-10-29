e3b479c43181507c4e2a3773d002c582685b0b6a
GuiVeigaSesiSenai <guilherme_v_pinto@estudante.sesisenai.org.br>
2021-10-29 20:44:42 -0300

    Correções e Implementação com Array

--- Ex 11 - Construa uma calculadora de P.A/scripts/script.js
+++ Ex 11 - Construa uma calculadora de P.A/scripts/script.js
@@ -1,20 +1,20 @@
-x = parseInt(window.prompt("Entre com o valor inicial", []));
-y = parseInt(window.prompt("Entre com o valor da raíz", []));
+var valorInicial = Number(prompt('digite o valor inicial'));
+
+const raiz = Number(prompt('Valor da raiz'));
 
 function btnClick() {
-    let resultado = window.confirm("Deseja visualizar as respostas?");
-
-    if (resultado == true) {
-
-        console.log("Valor inicial: " + x + "\nRaíz: " + y);
-
-        for (let i = 1; i <= 10; i++) {
-            x += y;
-            pa = x;
-            console.log("PA (" + i + "): " + pa);
-        }
+    if (isNaN(valorInicial) || isNaN(raiz)) {
+        alert('Digite somente números');
     }
     else {
-        window.alert("Confirmação recusada");
+        var arrayPa = [valorInicial];
+
+        for (let i = 0; i < 9; i++) {
+            valorInicial += raiz
+
+            arrayPa.push(valorInicial);
+
+        }
+        alert(arrayPa);
     }
 }
